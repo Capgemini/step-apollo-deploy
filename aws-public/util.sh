ba2425207ac31bd0a36d466ef773efb8bb1a8c47
enxebre <alberto.garcial@hotmail.com>
2015-07-24 00:24:41 +0100

    ssh key name should overriden from outside if needed.

--- aws-public/util.sh
+++ aws-public/util.sh
@@ -3,7 +3,6 @@
 function setup_ssh_config() {
 	pushd ~/.ssh
 	ssh-keygen -P "" -t rsa -f id_rsa_tmp -b 4096 -C "email@example.com"
-	export TF_VAR_key_name="deployer"
 	export TF_VAR_key_file='~/.ssh/id_rsa_tmp.pub'
 	openssl rsa -in ~/.ssh/id_rsa_tmp -outform pem > id_rsa_tmp.pem
 	chmod 400 id_rsa_tmp.pem
